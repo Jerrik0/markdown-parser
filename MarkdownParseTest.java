cfc988c311f8f4eb907782081dc5f02c28ae2743
Jerrik0 <pko@ucsd.edu>
2022-05-26 21:09:18 -0700

    Added test for lab report

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,11 @@
 //import tools for tests
+import static org.junit.Assert.*; 
+import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import static org.junit.Assert.*;
 import java.util.ArrayList;
-import org.junit.*;
+import java.util.List;
 
 public class MarkdownParseTest {
     //create a test
@@ -16,7 +17,7 @@ public class MarkdownParseTest {
     }
 
     @Test
-    public void testFile() {
+    public void testFile() throws IOException{
         Path fileName = Path.of("test-file.md");
         String content = Files.readString(fileName);
         ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
